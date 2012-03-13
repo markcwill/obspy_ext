53cf3237ccef209817fbe7cb4bb7446bb8881c3d
Mark Williams <mwilliams@coas.oregonstate.edu>
2012-03-13 16:10:38 -0700

    Split core and utilities

--- antelope/__init__.py
+++ antelope/__init__.py
@@ -5,5 +5,5 @@
 obspy antelope module
 """
 
-from obspy_ext.antelope.core import (Dbrecord, Dbview, DbrecordPtr,
-DbviewPtr, AttribDbptr, db2object, readANTELOPE)
+from obspy_ext.antelope.core import (Dbrecord, Dbview, db2object, readANTELOPE)
+from obspy_ext.antelope.dbutils import (DbrecordPtr, DbviewPtr, AttribDbptr)
